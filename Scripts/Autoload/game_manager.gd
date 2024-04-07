4dbc97a8f4c092719c94f0c04ae8e03692f609f8
Mitchell <mitchellswinn@gmail.com>
2024-04-06 20:05:02 -0700

    Main scene and project organization

---
+++ Scripts/Autoload/game_manager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	toggleFullscreen()
+		
+func toggleFullscreen():
+	if Input.is_action_just_pressed("fullscreen"):
+		if DisplayServer.window_get_mode(0) == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED, 0)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN, 0)
+
---
+++ Scripts/ButtonProperties.gd
@@ -0,0 +1,9 @@
+extends Button
+
+func _on_mouse_entered():
+	pass # Replace with function body.
+	modulate = Color(1,1,1,1)
+
+func _on_mouse_exited():
+	pass # Replace with function body.
+	modulate = Color(1,1,1,.5)
