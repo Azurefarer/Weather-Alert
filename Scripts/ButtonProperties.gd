c48d0500f5c43c1accee482b31ab51f461257bf7
AzureFarer <benjijdouglas@gmail.com>
2024-04-09 11:36:18 -0700

    Adding some VFX to the title screen

--- Scripts/ButtonProperties.gd
+++ Scripts/ButtonProperties.gd
@@ -2,9 +2,16 @@ extends Button
 
 func _on_mouse_entered():
 	GameManager.activeButton = self
+	# Cool shader effects
+	$"../BG".material.set("shader_parameter/hover", self.position)
+	# Cool shader effects
+	
 	pass # Replace with function body.
 	modulate = Color(1,1,1,1)
 
 func _on_mouse_exited():
+	# Cool shader effects
+	$"../BG".material.set("shader_parameter/hover", Vector2(0.0, 0.0))
+	# Cool shader effects
 	pass # Replace with function body.
 	modulate = Color(1,1,1,.5)
